b8f728bbde6115e573bec536477df850b36b95ad
shazadasalman <shazada.s4@samsung.com>
2023-06-06 11:30:45 +0530

    d

--- playground/3-arrow-challenge.js
+++ playground/3-arrow-challenge.js
@@ -5,20 +5,48 @@
 // 2. Use filter to to return just the incompleted tasks (arrow function)
 // 3. Test your work by running the script
 
+// const tasks = {
+//     tasks: [{
+//         text: 'Grocery shopping',
+//         completed: true
+//     }, {
+//         text: 'Clean yard',
+//         completed: false
+//     }, {
+//         text: 'Film course',
+//         completed: false
+//     }],
+//     getTasksToDo() {
+//         return this.tasks.filter((task) => task.completed === false)
+//     }
+// }
+//
+// console.log(tasks.getTasksToDo())
+
+//---------------------------------------------------------------
 const tasks = {
-    tasks: [{
+    tasks:[{
         text: 'Grocery shopping',
         completed: true
     }, {
         text: 'Clean yard',
         completed: false
-    }, {
-        text: 'Film course',
-        completed: false
-    }],
-    getTasksToDo() {
-        return this.tasks.filter((task) => task.completed === false)
+    }, {text: 'Film course', completed: false}],
+
+    getTaskList(){
+        this.tasks.forEach((task)=>{
+            if(task.completed === true)
+                console.log(task.text)
+        })
+    },
+
+    getIncompleteTask(){
+        return this.tasks.filter((task)=> {
+            return task.completed === false;
+        })
     }
+
 }
 
-console.log(tasks.getTasksToDo())
+tasks.getTaskList()
+console.log(tasks.getIncompleteTask())
